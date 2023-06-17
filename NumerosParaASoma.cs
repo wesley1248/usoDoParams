c6ace42dda2d0c45cca1d98c90f8b14aac4116d9
wesley1248 <wesleytms.es@gmail.com>
2023-06-17 20:39:14 -0300

    up

---
+++ NumerosParaASoma.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params {
+    internal class NumerosParaASoma {
+        // Método que recebe uma quantidade variável de números como parâmetros
+        public static void Array(params int[] numeros)
+        {
+            // Solicita ao usuário que digite os números separados por vírgulas
+            Console.WriteLine("Digite abaixo os numeros separados por virgulas(,)");
+            Console.Write("Numeros: ");
+            string entrada = Console.ReadLine();
+            string[] numerosString = entrada.Split(',');
+
+            // Converte os números fornecidos pelo usuário em um array de inteiros
+            numeros = new int[numerosString.Length];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = int.Parse(numerosString[i]);
+            }
+
+            // Variável para armazenar a soma dos números
+            int resultadoDaSoma = 0;
+
+            // Calcula a soma dos números fornecidos
+            foreach (int numero in numeros)
+            {
+                resultadoDaSoma += numero;
+            }
+
+            // Exibe o resultado da soma
+            Console.WriteLine("A soma dos numeros é: " + resultadoDaSoma);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Params {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            // Exibe uma mensagem de boas-vindas e solicita ao usuário que digite os números separados por vírgula
+            Console.WriteLine("Ola, digite abaixo separado por virgula (,) os números para serem somados!!");
+            
+            // Chama o método "Array" da classe "NumerosParaASoma" para realizar a soma dos números
+            NumerosParaASoma.Array();
+            
+            // Aguarda o usuário pressionar uma tecla antes de encerrar o programa
+            Console.ReadKey();
+        }
+    }
+}
